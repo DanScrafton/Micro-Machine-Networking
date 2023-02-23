67a89b776e664434dc396e86e8b83bedb612a3ca
Daniel SCRAFTON <b1046430@linux36.scedt.tees.ac.uk>
2023-02-23 11:59:53 +0000

    added tcp code

--- main.cpp
+++ main.cpp
@@ -3,187 +3,193 @@
 
 #include <cmath>
 #include <iostream>
+#include <thread>
+#include <vector>
+#include "My_Class.h"
 
-using namespace sf;
-
-const int num=8; //checkpoints
-// TODO: use checkpoint to make sure we are on the track.
-// Slow speed when not on the track.
-int points[num][2] = {300, 610,
-    1270,430,
-    1380,2380,
-    1900,2460,
-    1970,1700,
-    2550,1680,
-    2560,3150,
-    500, 3300};
-
-struct Car
+void Server()
 {
-    float x,y,speed,angle; int n;
-    Car() {speed=2; angle=0; n=0;}
-    void move()
-    {
-        x += sin(angle) * speed;
-        y -= cos(angle) * speed;
-    }
-    void findTarget()
-    {
-        float tx=points[n][0];
-        float ty=points[n][1];
-        float beta = angle-atan2(tx-x,-ty+y);
-        if (sin(beta)<0) angle+=0.005*speed; else angle-=0.005*speed;
-        // Check if passed a checkpoint
-        if ((x-tx)*(x-tx)+(y-ty)*(y-ty)<25*25) n=(n+1)%num; // TODO: simplify
-    }
-};
+sf::TcpListener listener;
+sf::Socket::Status status;
+status = listener.listen(4300);
+
+if(status != sf::Socket::Done)
+{
+std::cout<< "Error Listening\n";
+return;
+}
+sf::TcpSocket socket;
+status = lister.accept(socket); // blocking
+
+if(status != sf::Socket::Done)
+{
+std::cout<<"Error accepting\n"
+return;
+}
+
+status = socket.receive(packet);
+
+if(status != sf::Socket::Done)
+{
+std::cout<<"Error recieving\n";
+return;
+}
+
+std::string message;
+packet >> message;
+std::cout<<message;
+packet.clear();
+status = socket.send(packet);
+
+id(status !)
+}
+
 
 int main()
 {
-    // ****************************************
-    // Initialise
     srand(time(NULL));
-    RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
+//    std::thrsrand(time(NULL));
+//    std::thread t(Hello);
+//    t.join();
+//
+//    std::vector<std::thread>threads;
+//  srand(time(NULL));
+//    std::thread t(Hello);
+//    t.join();
+//
+//    std::vector<std::thread>threads;
+//    for(int i = 0; i<5; ++i)
+//    {
+//        threads.push_back(std::thread(Hello));
+//        std::this_thread::sleep_for(std::chrono::microseconds(5));
+//    }
+//
+//    for(auto& thread:threads)
+//    {
+//    thread.join();
+//    }
+//
+//    My_Class my_obj(5);
+//
+//    std::thread t1{&My_Class::Do_Something
+//    ,&my_obj
+//    ,10 // parameter 1
+//    ,-8 // parameter 2
+//    ,56}; // parameter 3
+//t1.join();
 
-    RectangleShape checkpoint1(Vector2f(400, 20));
-    checkpoint1.setFillColor(sf::Color::Green);
+//    std::thread t([]{
+//        std::cout << "Hi from thread\n";
+//    });
 
-    app.setFramerateLimit(60);
-    Texture t1,t2;
-    t1.loadFromFile("images/background.png");
-    t2.loadFromFile("images/car.png");
-    t1.setSmooth(true);
-    t2.setSmooth(true);
-    Sprite sBackground(t1), sCar(t2);
-    sBackground.scale(2,2);
-    sCar.setOrigin(22, 22);
-    float R=22;
-    const int N=5;
-    Car car[N];
-    Color colors[5] = {Color::Red, Color::Green, Color::Magenta, Color::Blue, Color::White};
+//    Print print;
 
-    // Starting positions
-    for(int i=0;i<N;i++)
-    {
-        car[i].x=300+i*50;
-        car[i].y=1700+i*80;
-        car[i].speed=7+((float)i/5);
-    }
-    float speed=0,angle=0;
-    float maxSpeed=12.0;
-    float acc=0.2, dec=0.3;
-    float turnSpeed=0.08;
-    int offsetX=0,offsetY=0;
+//    print.print();
 
+//    std::thread t1(&Print::print, &print);
+//    std::thread t1([&print]{print.print();});
 
-    // ****************************************
-    // Loop
+//    t.join();
+//    t1.join();
 
-    while (app.isOpen())
-    {
-        Event e;
-        while (app.pollEvent(e))
-        {
-            if (e.type == Event::Closed)
-                app.close();
-        }
-        // Step 1: user input
-        bool Up=0,Right=0,Down=0,Left=0;
-        if (Keyboard::isKeyPressed(Keyboard::Up))    Up=1;
-        if (Keyboard::isKeyPressed(Keyboard::Right)) Right=1;
-        if (Keyboard::isKeyPressed(Keyboard::Down))  Down=1;
-        if (Keyboard::isKeyPressed(Keyboard::Left))  Left=1;
-        // alternative
-//  bool Up=0,Right=0,Down=0,Left=0;
-        if(Keyboard::isKeyPressed(Keyboard::W)) Up=1;
-        if(Keyboard::isKeyPressed(Keyboard::D)) Right=1;
-        if(Keyboard::isKeyPressed(Keyboard::S)) Down=1;
-        if(Keyboard::isKeyPressed(Keyboard::A)) Left=1;
-        //
-        // Step 2: update
-        //car movement
-        if (Up && speed<maxSpeed)
-        {
-            if (speed < 0)  speed += dec;
-            else  speed += acc;
-        }
-        if (Down && speed>-maxSpeed)
-        {
-            if (speed > 0) speed -= dec;
-            else  speed -= acc;
-        }
-        if (!Up && !Down)
-        {
-            if (speed - dec > 0) speed -= dec;
-            else if (speed + dec < 0) speed += dec;
-            else speed = 0;
-        }
-        if (Right && speed!=0)  angle += turnSpeed * speed/maxSpeed;
-        if (Left && speed!=0)   angle -= turnSpeed * speed/maxSpeed;
-        car[0].speed = speed;
-        car[0].angle = angle;
-        for(int i=0;i<N;i++) car[i].move();
-        for(int i=1;i<N;i++) car[i].findTarget();
-        //collision
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<N;j++)
-            {
-                if (i == j)
-                {
-                    break;
-                }
-                int dx=0, dy=0;
-                while (dx*dx+dy*dy<4*R*R)
-                {
-                    car[i].x+=dx/10.0;
-                    car[i].x+=dy/10.0;
-                    car[j].x-=dx/10.0;
-                    car[j].y-=dy/10.0;
-                    dx = car[i].x-car[j].x;
-                    dy = car[i].y-car[j].y;
-                    if (!dx && !dy) break;
-                }
-            }
-        }
-        // Step 3: Render
-        app.clear(Color::White);
-        // TODO: Stay within the limit of the map.
-        if (!sBackground.getGlobalBounds().contains(car[0].x,car[0].y))
-        {
-        // return to track
+    //Loop loop;
+    //std::thread tLoop(&Loop::run, &loop);
+    //std::thread tStop([&loop]{
+      //  std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 10000));
+       // loop.hasToStop = true;
+    //});
+    //tStop.detach();
 
-        // clipping and blitting to look at
-        // if centre off tweak this
+    //tLoop.join();
+    //return 0;  for(int i = 0; i<5; ++i)
+//    {
+//        threads.push_back(std::thread(Hello));
+//        std::this_thread::sleep_for(std::chrono::microseconds(5));
+//    }
+//
+//    for(auto& thread:threads)
+//    {
+//    thread.join();
+//    }
+//
+//    My_Class my_obj(5);
+//
+//    std::thread t1{&My_Class::Do_Something
+//    ,&my_obj
+//    ,10 // parameter 1
+//    ,-8 // parameter 2
+//    ,56}; // parameter 3
+//t1.join();
 
-        //   if (Car[0].x > sBackground.getGlobalBounds().width)
-           //    Car[0].x = sBackground.getGlobalBounds().width;
-          //if (Car[0].x < sBackground.getGlobalBounds().width)
-          //  Car[0].x = sBackground.getGlobalBounds().width;
-           // if (Car[0].y < sBackground.getGlobalBounds().height)
-             //   Car[0].y = sBackground.getGlobalBounds().height;
-               //  if (Car[0].y > sBackground.getGlobalBounds().height)
-               // Car[0].y = sBackground.getGlobalBounds().height;
-        }
-        // TODO: Don't show white at bottom/right.
-        if (car[0].x>320) offsetX = car[0].x-320;
-        if (car[0].y>240) offsetY = car[0].y-240;
-        sBackground.setPosition(-offsetX,-offsetY);
-        checkpoint1.setPosition(400 - offsetX, 600 - offsetY);
+//    std::thread t([]{
+//        std::cout << "Hi from thread\n";
+//    });
 
-//        checkpoint1.getGlobalBounds().intersects()
+//    Print print;
 
-        app.draw(sBackground);
-        app.draw(checkpoint1);
-        for(int i=0;i<N;i++)
-        {
-            sCar.setPosition(car[i].x-offsetX,car[i].y-offsetY);
-            sCar.setRotation(car[i].angle*180/3.141593);
-            sCar.setColor(colors[i]);
-            app.draw(sCar);
-        }
-        app.display();
-    }
+//    print.print();
 
+//    std::thread t1(&Print::print, &print);
+//    std::thread t1([&print]{print.print();});
+
+//    t.join();
+//    t1.join();
+
+   // Loop loop;
+    //std::thread tLoop(&Loop::run, &loop);
+   // std::thread tStop([&loop]{
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 10000));
+     //   loop.hasToStop = true;
+    //});
+   // tStop.detach();
+
+    //tLoop.join();
+    //return 0;ead t(Hello);
+//    t.join();
+//
+//    std::vector<std::thread>threads;
+//    for(int i = 0; i<5; ++i)
+//    {
+//        threads.push_back(std::thread(Hello));
+//        std::this_thread::sleep_for(std::chrono::microseconds(5));
+//    }
+//
+//    for(auto& thread:threads)
+//    {
+//    thread.join();
+//    }
+//
+//    My_Class my_obj(5);
+//
+//    std::thread t1{&My_Class::Do_Something
+//    ,&my_obj
+//    ,10 // parameter 1
+//    ,-8 // parameter 2
+//    ,56}; // parameter 3
+//t1.join();
+
+//    std::thread t([]{
+//        std::cout << "Hi from thread\n";
+//    });
+
+//    Print print;
+
+//    print.print();
+
+//    std::thread t1(&Print::print, &print);
+//    std::thread t1([&print]{print.print();});
+
+//    t.join();
+//    t1.join();
+
+    //Loop loop;
+    //std::thread tLoop(&Loop::run, &loop);
+    //std::thread tStop([&loop]{
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 10000));
+    //    loop.hasToStop = true;
+    //});
+    //tStop.detach();
+
+    //tLoop.join();
     return 0;
 }
