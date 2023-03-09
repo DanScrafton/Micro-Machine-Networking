7ee8db608732d938087a70aaa9397b1d40debd5a
Daniel SCRAFTON <b1046430@linux36.scedt.tees.ac.uk>
2023-03-09 12:47:43 +0000

    lab session 9/3/23

---
+++ Server.h
@@ -0,0 +1,10 @@
+#ifndef SERVER_H_INCLUDED
+#define SERVER_H_INCLUDED
+
+class Server {
+public:
+    void run();
+};
+
+
+#endif // SERVER_H_INCLUDED
---
+++ client.cpp
@@ -0,0 +1,29 @@
+#include "Client.h"
+#include "queue.h"
+#include "receiver.h"
+#include "util.h"
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <thread>
+
+void Client::run()
+{
+    std::shared_ptr<sf::TcpSocket> socket = std::make_shared<sf::TcpSocket>();
+    // auto connect = [&] { return socket->connect(sf::IpAddress::getLocalAddress(), PORT); };
+    // net_run(connect, "connect");
+    sf::Socket::Status status = socket->connect(sf::IpAddress::getLocalAddress(), PORT);
+    if (status != sf::Socket::Done) {
+        return;
+    }
+    std::cout << "Connected\n";
+    Queue<std::string> queue;
+    // TODO launch a receiver thread to receive messages from the server.
+    std::string s = "";
+    while (1)
+    {
+        std::getline(std::cin, s);
+        std::cout << "Sending: \"" << s << "\"" << std::endl;
+        // TODO send messages to the server
+    }
+    return;
+}
